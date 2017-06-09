72225b2c1c202d2f96abaf89e34124756472a83f
Sebastiano Ferraris <sebastiano.ferraris@gmail.com>
2017-06-09 16:38:46 +0100

    typos

--- run_python.sh
+++ run_python.sh
@@ -2,5 +2,5 @@
 
 # path to your selected python interpreter located on the cluster.
 # /Applications/python_virtualenvs/clean2_7/bin/python
-/home/ferraris/py_venvs/v2/bin/ main/main.py -i ${1}
+/home/ferraris/py_venvs/v2/bin/python main/main.py -i ${1}
 
